7dd369fb297b19b005448341ca6c4a6a0c1c596c
angeladaddy <sunchenge1997@sjtu.edu.cn>
2020-05-23 23:27:08 +0800

    为了在论文里解释我又重构了

--- simple/node.go
+++ simple/node.go
@@ -10,18 +10,19 @@ const (
 	P_FAIL=0.1
 	GOSSIP_INTERVAL=time.Second //1s发一次
 	REPAIR_TIME=4*time.Second
-	NODE_NUM = 6
-	BUFSIZE = 5 //channel buffer size 一般设置为数据中心节点的数目即可
+	NODE_NUM = 5
+	BUFSIZE = 4 //channel buffer size 一般设置为数据中心节点的数目即可
 	K = 2
-	IS_AUTO = true
+	IS_AUTO = false
 )
 
+type Nil struct {}
 
 type Node struct {
 	isbad bool
 	trans chan []Message
-	time chan bool
-	timeout chan bool
+	time chan Nil
+	timeout chan Nil
 	accept chan []Message
 	deliver chan []Message
 	Others []*Node
@@ -35,8 +36,8 @@ func NewNode(address string) (instance *Node) {
 	instance.accept = make(chan []Message)
 	instance.deliver = make(chan []Message)
 	instance.trans = make(chan []Message, BUFSIZE)
-	instance.time = make(chan bool)
-	instance.timeout = make(chan bool)
+	instance.time = make(chan Nil)
+	instance.timeout = make(chan Nil)
 	instance.isbad = false
 	instance.address = address
 	instance.Others = make([]*Node, 0)
@@ -50,7 +51,7 @@ func (node *Node) Fragile() {
 	fmt.Printf("Node %p Starts\n", node)
 	go node.timer()
 	go node.membership.Running()
-	node.time <- true
+	node.time <- Nil{}
 	for {
 		if (IS_AUTO) {
 			node.Bad()
@@ -68,7 +69,13 @@ func (node *Node) Fragile() {
 			}
 		case <- node.timeout:
 			messages := <- node.accept
-			node.Gossiping(messages)
+			rand.Seed(time.Now().UnixNano())
+			perm := rand.Perm(len(node.Others))[:K]
+			var targets []*Node
+			for _, p := range perm {
+				targets = append(targets, node.Others[p])
+			}
+			node.Gossiping(messages, targets)
 		}
 	}
 }
@@ -81,27 +88,28 @@ func (node *Node) Bad() {
 			node.isbad = false
 		}
 }
+
 func (node *Node) timer() {
 	for {
 		select {
 		case <- node.time:
 			time.Sleep(GOSSIP_INTERVAL)
-			node.timeout <- true
+			node.timeout <- Nil{}
 		}
 	}
 }
 
-func (node *Node) Gossiping(messages []Message) {
-	rand.Seed(time.Now().UnixNano())
-	//随机全排列，取前K个
-	perm := rand.Perm(len(node.Others))[:K]
-	var targets []*Node
-	for _, p := range perm {
-		targets = append(targets, node.Others[p])
+func (node *Node) Gossiping(messages []Message, targets []*Node) {
+	var str string
+	for _, t := range targets {
+		if (len(t.trans) == BUFSIZE) {
+			continue
+		}
+		t.trans <- messages
+		str+=(t.address+" ")
 	}
-	str := transmitting(messages, targets)
 	fmt.Printf("[SEND] From: %s; To: %s\n", node.address, str)
-	node.time <- true
+	node.time <- Nil{}
 }
 
 func transmitting(messages []Message, targets []*Node) string {
